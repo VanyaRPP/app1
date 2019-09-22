b7770aba9e717e2fc91b4965ac0869b9987ec918
Vanya <51020174+VanyaRPP@users.noreply.github.com>
2019-09-22 15:06:30 +0300

    1

--- myapp1/src/index.js
+++ myapp1/src/index.js
@@ -1,35 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import state from "./redux/state";
 
-let postData = [
-    { id: 1, mes: 'HI.HOW ARE YOU', like: 15, dlike: 3 },
-    { id: 2, mes: 'HI.', like: 345, dlike: 457 },
-    { id: 3, mes: 'HI.LLL', like: 4357, dlike: 475 },
-    { id: 4, mes: 'HI.', like: 475, dlike: 547 },
-    { id: 5, mes: 'HI.LOL', like: 457, dlike: 154 },
-    { id: 6, mes: 'Vanya', like: 574, dlike: 153 },
-]
-let dialogData = [
-    { id: 1, name: 'Vanya' },
-    { id: 2, name: 'Sanya' },
-    { id: 3, name: 'Kolya' },
-    { id: 4, name: 'Zenya' },
-    { id: 5, name: 'Tolya' },
-    { id: 6, name: 'Uma' },
-]
-let MessageData = [
-    { id: 1, Message: 'Hi!' },
-    { id: 2, Message: 'Hi! How are you?' },
-    { id: 3, Message: 'Hi!' },
-    { id: 4, Message: 'Hi! How are you?' },
-    { id: 5, Message: 'Hi!' },
-    { id: 6, Message: 'Hi! How are you?' },
-    { id: 7, Message: 'Hi!Vanyuafsk)' },
-]
-ReactDOM.render(<App postData={postData} dialogData={dialogData} MessageData={MessageData} />, document.getElementById('root'));
+ReactDOM.render(<App state={state} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ myapp1/src/redux/state.js
@@ -0,0 +1,28 @@
+let state = {
+  post: [
+    { id: 1, mes: "HI.HOW ARE YOU", like: 15, dlike: 3 },
+    { id: 2, mes: "HI.", like: 345, dlike: 457 },
+    { id: 3, mes: "HI.LLL", like: 4357, dlike: 475 },
+    { id: 4, mes: "HI.", like: 475, dlike: 547 },
+    { id: 5, mes: "HI.LOL", like: 457, dlike: 154 },
+    { id: 6, mes: "Vanya", like: 574, dlike: 153 }
+  ],
+  dialog: [
+    { id: 1, name: "Vanya" },
+    { id: 2, name: "Sanya" },
+    { id: 3, name: "Kolya" },
+    { id: 4, name: "Zenya" },
+    { id: 5, name: "Tolya" },
+    { id: 6, name: "Uma" }
+  ],
+  message: [
+    { id: 1, Message: "Hi!" },
+    { id: 2, Message: "Hi! How are you?" },
+    { id: 3, Message: "Hi!" },
+    { id: 4, Message: "Hi! How are you?" },
+    { id: 5, Message: "Hi!" },
+    { id: 6, Message: "Hi! How are you?" },
+    { id: 7, Message: "Hi!Vanyuafsk)" }
+  ]
+};
+export default state;
